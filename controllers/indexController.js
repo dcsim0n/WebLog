454e984dabcfefeccad1a0de8cf6c513f3f54dc5
Dana Simmons <dsanalog@gmail.com>
2019-09-22 10:16:00 -0400

    Build log view and Contact model
    Created main log view and a contact model that reads/writes to
    contacts.json.

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -4,9 +4,13 @@
 |--------------------------------------------------
 */
 
+const Contact = require('../models/contact');
 
 exports.indexPage = function(req, res, next) {
 
+  Contact.allContacts( ( contacts ) => {
+    
+    res.render('index', { title: 'Main Log', contacts });
 
-  res.render('index', { title: 'Main Log' });
+  });
 }
--- models/contact.js
+++ models/contact.js
@@ -1,7 +1,23 @@
 const fs = require('fs');
-const SCHEMA = require('../schema.json');
 
-exports = class Contact {
+const path = require('path');
+
+const p = path.join( path.dirname(process.mainModule.filename), "contacts.json" );
+function readContacts( cb ){
+  fs.readFile(p , (err, data) =>{
+    if(!err) {
+      parsedData = JSON.parse( data );
+      cb( parsedData );
+    }else{
+      throw err
+    }
+  })
+}
+
+function writeContacts( data, cb ){
+  fs.writeFile(p, data, cb );
+}
+module.exports = class Contact {
   
   constructor( args ){
     Object.keys( args ).forEach( key => {
@@ -9,11 +25,18 @@ exports = class Contact {
     });
   }
 
-  static allContacts(cb) {
-    fs.readFile('../contact.json', (err, data) =>{
-      if(!err) {
-        cb( data )
-      }
+  save( ){
+    row = { id: this.id, date: this.date, freq: this.freq, operator: this.operator, dx: this.dx, rst: this.rst }
+    readContacts( ( contacts )=>{
+      contacts.push(row);
+      writeContacts( contacts );
     })
+  }
+
+  static allContacts(cb) {
+    console.log("Getting contacts..")
+    readContacts( data =>{
+      cb ( data.map( row => new Contact( row )))
+    } );
   }
 }
